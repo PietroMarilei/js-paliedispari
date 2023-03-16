374096ea567a48b88ebd07d227b815680230891a
PietroMarilei~ <pietromrl0@gmail.com>
2023-03-16 15:20:05 +0100

    working odd or even part

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,85 @@
-const userWord = prompt("inserisci la parola")
-
-function reverser(word) {
-
-    const reverseWord = Array.from(word).reverse().join();
-
-    if (reverseWord == word) {
-        return true
-    }
-
-    return false
-
-}
-// ho dichiarato la funzione 
-
-
-const resultOfReverser = reverser(userWord)
-
-// uso la funzione e mi salvo il suo risultato in una variabile che poi posso richiamare
-
-console.log(resultOfReverser);
+// Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
 
 
 
+// const userWord = prompt("inserisci la parola")
+
+// function reverser(word) {
+
+//     const reverseWord = Array.from(word).reverse().join();
+//      console.log(word);
+//      console.log(reverseWord);
+
+//     if (reverseWord == word) {
+//         return true
+//     }
+
+//     return false
+
+// }
+// // ho dichiarato la funzione
+
+
+// const resultOfReverser = reverser(userWord)
+
+// // uso la funzione e mi salvo il suo risultato in una variabile che poi posso richiamare
+
+// console.log(resultOfReverser);
+
+// ----------------------------------------------------------------------------------
+
+// // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random(sempre da 1 a 5) per il computer(usando una funzione).Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari(usando una funzione) Dichiariamo chi ha vinto.
+
+
+// const userChoice = prompt("scegli pari o dispari");
+
+// const userNumber = Number(prompt("scegli un numero da 1 a 5"));
+// console.log(userNumber);
+// // chiedo all'utente i dati
+
+// function randomNumbGenerator(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+// // funzione di W3S per generare un numero random in un intervallo definito
+
+// const enemyNumber = randomNumbGenerator(1, 5)
+// console.log(enemyNumber);
+
+// // metto in una const il numero casuale del pc
+
+// const sumOfNumbers = userNumber + enemyNumber;
+// // sommo i due numeri
+
+// function isEven(number) {
+//     if (number % 2 == 0) {
+//         return true
+//     } else {
+//         return false
+//     }
+
+// }
+// // ho fatto una funzione che mi dice se un numero é pari o dispari
+
+// let resultOfIsEven = isEven(sumOfNumbers)
+// // immagazzino il risulato in una const
+
+// if (resultOfIsEven == true) {
+//     resultOfIsEven = "pari"
+//     console.log(resultOfIsEven);
+//     console.log("il risultato della somma dei due numeri é pari");
+
+// } else {
+//     resultOfIsEven = "dispari";
+//     console.log(resultOfIsEven);
+//     console.log("il risultato della somma dei due numeri é dispari");
+// }
+
+// //  fino a qua ho verifcato che la somma sia pari o dispari
+
+// if (userChoice == resultOfIsEven) {
+//     console.log("hai vinto");
+
+// } else {
+//     console.log("hai perso");
+// }
+// // ora siccome resultofisEven diventa una stringa chiamata pari o dispari a seconda del risultato se la userChoice corrisponde hai vinto, oppure hai perso. 
