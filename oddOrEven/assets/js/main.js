930630837cd5d5f776f14a148bdea28d2fe6abc1
PietroMarilei~ <pietromrl0@gmail.com>
2023-06-20 16:34:22 +0200

    initial commit

---
+++ oddOrEven/assets/js/main.js
@@ -0,0 +1,57 @@
+
+// const userChoice = prompt("scegli pari o dispari");
+
+// const userNumber = Number(prompt("scegli un numero da 1 a 5"));
+// console.log(userNumber);
+// // chiedo all'utente i dati
+
+// function randomNumbGenerator(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+// // funzione di W3S per generare un numero random in un intervallo definito
+
+// const enemyNumber = randomNumbGenerator(1, 5)
+// console.log(enemyNumber);
+
+// // metto in una const il numero casuale del pc
+
+// const sumOfNumbers = userNumber + enemyNumber;
+// // sommo i due numeri
+
+// function isEven(number) {
+//     if (number % 2 == 0) {
+//         return true
+//     } else {
+//         return false
+//     }
+
+//     return number % 2 == 0
+
+// }
+// // ho fatto una funzione che mi dice se un numero é pari o dispari
+
+// const resultOfIsEven = isEven(sumOfNumbers)
+// // immagazzino il risulato in una const
+// let flag
+
+// if (resultOfIsEven == true) {
+//     flag = "pari"
+//     console.log(resultOfIsEven);
+//     console.log("il risultato della somma dei due numeri é pari");
+
+// } else {
+//     flag = "dispari";
+//     console.log(resultOfIsEven);
+//     console.log("il risultato della somma dei due numeri é dispari");
+// }
+
+// //  fino a qua ho verifcato che la somma sia pari o dispari
+
+// if (userChoice == flag) {
+//     console.log("hai vinto");
+
+// } else {
+//     console.log("hai perso");
+// }
+
+
